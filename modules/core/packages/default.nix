c44a14560509f633f3bc14feb952713cb3218f56
annmaro <anandkumar60440@gmail.com>
2026-05-09 13:42:30 +0530

    added cursor IDE:   modules/core/packages/default.nix 	deleted:    modules/programs/editor/antigravity/default.nix 	deleted:    modules/programs/editor/cursor/default.nix

--- modules/core/packages/default.nix
+++ modules/core/packages/default.nix
@@ -48,6 +48,7 @@
     qbittorrent # Featureful free software BitTorrent client
     libreoffice-fresh # Comprehensive, professional-quality productivity suite
     android-tools # Android SDK platform tools
+    inputs.cursor.packages.${pkgs.stdenv.hostPlatform.system}.default # AI-powered code editor built on vscode
     # aider-chat # AI in terminal (Optional: Client only)
     # cmatrix # Matrix Movie Effect In Terminal
     # cowsay # Great Fun Terminal Program
