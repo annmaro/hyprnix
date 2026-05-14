3c62bdf5e8586904b84ee9dcdf0f0c43830fd670
annmaro <anandkumar60440@gmail.com>
2026-05-15 01:32:21 +0530

    modified:   overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,7 +14,7 @@ in
   modifications = final: _prev: {
     nur = inputs.nur.overlays.default;
     stable = import inputs.nixpkgs-stable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
