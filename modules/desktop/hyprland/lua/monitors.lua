b0e6e9de06bd0530a9a609a24847284cb2bc5a97
annmaro <anandk60440@gmail.com>
2026-05-21 14:49:16 +0530

    new lua config for animations :   modules/desktop/hyprland/lua/animations.lua 	new lua config for binds:   modules/desktop/hyprland/lua/binds.lua 	new lua config for hyprland:   modules/desktop/hyprland/lua/hyprland.lua 	new lua config fro monitors:   modules/desktop/hyprland/lua/monitors.lua 	new lua config for rules:   modules/desktop/hyprland/lua/rules.lua 	new lua config for settings:   modules/desktop/hyprland/lua/settings.lua

---
+++ modules/desktop/hyprland/lua/monitors.lua
@@ -0,0 +1,39 @@
+hl.monitor({
+	name = "eDP-1",
+	mode = "preferred",
+	offset = "0x0",
+	scale = 1,
+})
+--[[ Add extra monitors here, find desc with: hyprctl monitors
+hl.monitor({
+	output = "desc:BNQ BenQ EW277HDR 99J01861SL0",
+	mode = "preferred",
+	position = "-1920x0",
+	scale = 1,
+})
+hl.monitor({
+	output = "desc:BNQ BenQ EL2870U PCK00489SL0",
+	mode = "preferred",
+	position = "0x0",
+	scale = 2,
+})
+hl.monitor({
+	output = "desc:BNQ BenQ xl2420t 99D06760SL0",
+	mode = "preferred",
+	position = "1920x-420",
+	scale = 1,
+	transform = 1, -- 5 for fipped
+})
+--]]
+-- Binds workspaces to my monitors (find desc with: hyprctl monitors)
+hl.workspace_rule({ workspace = "1", persistent = true, monitor = "desc:BOE 0x0690", default = true })
+--[[hl.workspace_rule({ workspace = "2", persistent = true, monitor = "desc:BNQ BenQ EL2870U PCK00489SL0" })
+hl.workspace_rule({ workspace = "3", persistent = true, monitor = "desc:BNQ BenQ EL2870U PCK00489SL0" })
+hl.workspace_rule({ workspace = "4", persistent = true, monitor = "desc:BNQ BenQ EL2870U PCK00489SL0" })
+hl.workspace_rule({ workspace = "5", persistent = true, monitor = "desc:BNQ BenQ EW277HDR 99J01861SL0", default = true })
+hl.workspace_rule({ workspace = "6", persistent = true, monitor = "desc:BNQ BenQ EW277HDR 99J01861SL0" })
+hl.workspace_rule({ workspace = "7", persistent = true, monitor = "desc:BNQ BenQ EW277HDR 99J01861SL0" })
+hl.workspace_rule({ workspace = "8", persistent = true, monitor = "desc:BNQ BenQ xl2420t 99D06760SL0", default = true })
+hl.workspace_rule({ workspace = "9", persistent = true, monitor = "desc:BNQ BenQ xl2420t 99D06760SL0" })
+hl.workspace_rule({ workspace = "10", persistent = true, monitor = "desc:BNQ BenQ EL2870U PCK00489SL0" })
+--]]
