f09d40df0ae440ef3dc6810884d82af7ad6db387
annmaro <anandk60440@gmail.com>
2026-05-21 17:16:52 +0530

    modified:   modules/desktop/hyprland/scripts/rofimusic.nix

--- modules/desktop/hyprland/scripts/rofimusic.nix
+++ modules/desktop/hyprland/scripts/rofimusic.nix
@@ -1,20 +1,15 @@
 { pkgs, ... }:
+
 pkgs.writeShellApplication {
   name = "rofimusic";
 
-  # Nix automatically registers these dependencies on the run-path of the script,
-  # keeping our bash code clean, standard, and easy to maintain.
+  # Nix automatically bundles all required binaries into the script's local execution PATH
   runtimeInputs = with pkgs; [
-    procps
-    rofi
-    libnotify
-    mpv
-  ];
-
-  # Directives to bypass ShellCheck warnings inside the builder wrapper
-  excludeShellChecks = [
-    "SC2154" # Referenced but not assigned variables
-    "SC2034" # Unused variables (safeguards dynamic associative arrays)
+    coreutils    # Provides printf
+    procps       # Provides pidof, pkill
+    rofi         # Provides rofi
+    mpv          # Provides mpv
+    libnotify    # Provides notify-send
   ];
 
   text = ''
@@ -25,29 +20,28 @@ pkgs.writeShellApplication {
 
     iDIR="$HOME/.config/hypr/icons"
 
-    # We define the array using Bash hexadecimal escape codes.
-    # This prevents the Nix builder from crashing on UTF-8 emoji strings.
-    declare -A shuffle
-    shuffle["Pop "$'\U1F4FB\U1F3B6']="https://youtube.com/playlist?list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj"
-    shuffle["Dance "$'\U1F4FB\U1F3B6']="https://dancewave.online:443/dance.mp3"
-    shuffle["Lofi Radio "$'\u2615\U1F3B6']="https://play.streamafrica.net/lofiradio"
-    shuffle["96.3 Easy Rock "$'\U1F4FB\U1F3B6']="https://radio-stations-philippines.com/easy-rock"
-    shuffle["Rock "$'\U1F4FB\U1F3B6']="https://www.youtube.com/playlist?list=PL6Lt9p1lIRZ311J9ZHuzkR5A3xesae2pk"
-    shuffle["Ghibli Music "$'\U1F3BB\U1F3B6']="https://youtube.com/playlist?list=PLNi74S754EXbrzw-IzVhpeAaMISNrzfUy&si=rqnXCZU5xoFhxfOl"
-    shuffle["Top Youtube Music 2023 "$'\u2615\U1F3B6']="https://youtube.com/playlist?list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU&si=y7qNeEVFNgA-XxKy"
-    shuffle["Chillhop "$'\u2615\U1F3B6']="https://stream.zeno.fm/fyn8eh3h5f8uv"
-    shuffle["SmoothChill "$'\u2615\U1F3B6']="https://media-ssl.musicradio.com/SmoothChill"
-    shuffle["Smooth UK "$'\u2615\U1F3B6']="https://icecast.thisisdax.com/SmoothUKMP3"
-    shuffle["Relaxing Music "$'\u2615\U1F3B6']="https://youtube.com/playlist?list=PLMIbmfP_9vb8BCxRoraJpoo4q1yMFg4CE"
-    shuffle["Youtube Remix "$'\U1F4FB\U1F3B6']="https://youtube.com/playlist?list=PLeqTkIUlrZXlSNn3tcXAa-zbo95j0iN-0"
-    shuffle["_Headbangers "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL27g7BfUwAEoBr2Cr5EY0aP8"
-    shuffle["_Motorway "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL2613eXf-20WT6VQnZenrg0X"
-    shuffle["_Carriageway "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL26qNYOBo0_9yW9za1Egwp_y"
-    shuffle["_Classics "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL260MDLEfAej9CqFqdycTf3X"
-    shuffle["_Metal "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL246iFzN3q8-cYCA43YBxv_z"
-    shuffle["_Limo "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL27x3iZrv2ElvTK7-iQzQKYY"
-    shuffle["_80s 90s 2000s "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL24FAtYVcivVfHImRsu-ocj4"
-    shuffle["_Hard Rock "$'\U1F3B5']="https://youtube.com/playlist?list=PLLosUj2DlL25A5u32lnZXtc_AUy-u2AUd"
+    declare -A shuffle=(
+      ["Pop 📻🎶"]="https://youtube.com/playlist?list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj"
+      ["Dance 📻🎶"]="https://dancewave.online:443/dance.mp3"
+      ["Lofi Radio ☕️🎶"]="https://play.streamafrica.net/lofiradio"
+      ["96.3 Easy Rock 📻🎶"]="https://radio-stations-philippines.com/easy-rock"
+      ["Rock 📻🎶"]="https://www.youtube.com/playlist?list=PL6Lt9p1lIRZ311J9ZHuzkR5A3xesae2pk"
+      ["Ghibli Music 🎻🎶"]="https://youtube.com/playlist?list=PLNi74S754EXbrzw-IzVhpeAaMISNrzfUy&si=rqnXCZU5xoFhxfOl"
+      ["Top Youtube Music 2023 ☕️🎶"]="https://youtube.com/playlist?list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU&si=y7qNeEVFNgA-XxKy"
+      ["Chillhop ☕️🎶"]="https://stream.zeno.fm/fyn8eh3h5f8uv"
+      ["SmoothChill ☕️🎶"]="https://media-ssl.musicradio.com/SmoothChill"
+      ["Smooth UK ☕️🎶"]="https://icecast.thisisdax.com/SmoothUKMP3"
+      ["Relaxing Music ☕️🎶"]="https://youtube.com/playlist?list=PLMIbmfP_9vb8BCxRoraJpoo4q1yMFg4CE"
+      ["Youtube Remix 📻🎶"]="https://youtube.com/playlist?list=PLeqTkIUlrZXlSNn3tcXAa-zbo95j0iN-0"
+      ["_Headbangers 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL27g7BfUwAEoBr2Cr5EY0aP8"
+      ["_Motorway 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL2613eXf-20WT6VQnZenrg0X"
+      ["_Carriageway 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL26qNYOBo0_9yW9za1Egwp_y"
+      ["_Classics 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL260MDLEfAej9CqFqdycTf3X"
+      ["_Metal 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL246iFzN3q8-cYCA43YBxv_z"
+      ["_Limo 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL27x3iZrv2ElvTK7-iQzQKYY"
+      ["_80s 90s 2000s 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL24FAtYVcivVfHImRsu-ocj4"
+      ["_Hard Rock 🎵"]="https://youtube.com/playlist?list=PLLosUj2DlL25A5u32lnZXtc_AUy-u2AUd"
+    )
 
     declare -A menu_options
     for key in "''${!shuffle[@]}"; do 
@@ -60,7 +54,7 @@ pkgs.writeShellApplication {
 
     main() {
       r_override="entry{placeholder:'Search Music...';}listview{lines:10;}"
-      choice=$(printf "%s\n" "''${!menu_options[@]}" | rofi -dmenu -theme-str "$r_override" -theme ~/.config/rofi/launchers/type-2/style-2.rasi -i -p "")
+      choice=$(printf "%s\n" "''${!menu_options[@]}" | rofi -dmenu -theme-str "$r_override" -theme "$HOME"/.config/rofi/launchers/type-2/style-2.rasi -i -p "")
 
       if [ -z "$choice" ]; then
         exit 1
@@ -77,6 +71,11 @@ pkgs.writeShellApplication {
       fi
     }
 
-    pkill mpv && notify-send -e -t 2500 -u low -i "$iDIR/music.png" "Playback stopped" || main
+    # FIXED: Replaced unsafe conditional shorthand pipeline (SC2015) with strict if/else branching
+    if pkill mpv; then
+      notify-send -e -t 2500 -u low -i "$iDIR/music.png" "Playback stopped"
+    else
+      main
+    fi
   '';
 }
