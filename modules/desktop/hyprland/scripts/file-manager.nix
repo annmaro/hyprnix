ad6451679689704fbca258c6ec76a19a333d9f2c
annmaro <anandk60440@gmail.com>
2026-05-21 16:15:12 +0530

    modified:   modules/desktop/hyprland/scripts/file-manager.nix

--- modules/desktop/hyprland/scripts/file-manager.nix
+++ modules/desktop/hyprland/scripts/file-manager.nix
@@ -3,34 +3,25 @@
 pkgs.writeShellApplication {
   name = "file-manager";
 
-  # Nix ensures these GUI tools and terminal file managers are available in the script PATH
+  # Nix will safely bundle Thunar and Yazi into the script's execution path
   runtimeInputs = with pkgs; [
     coreutils
     thunar
-    dolphin
-    nautilus
-    pcmanfm
-    nemo
     yazi
-    lf
-    nnn
-    ranger
-    mc
   ];
 
   text = ''
     case "$1" in
-      thunar)   thunar ;;
-      dolphin)  dolphin ;;
-      nautilus) nautilus ;;
-      pcmanfm)  pcmanfm ;;
-      nemo)     nemo ;;
-      yazi)     ${terminal} --class "tuiFileManager" -e yazi ;;
-      lf)       ${terminal} --class "tuiFileManager" -e lf ;;
-      nnn)      ${terminal} --class "tuiFileManager" -e nnn ;;
-      ranger)   ${terminal} --class "tuiFileManager" -e ranger ;;
-      mc)       ${terminal} --class "tuiFileManager" -e mc ;;
-      *)        echo "Unknown file manager layout profile requested: $1"; exit 1 ;;
+      thunar)
+        thunar
+        ;;
+      yazi)
+        ${terminal} --class "fileManager" -e yazi
+        ;;
+      *)
+        echo "Unknown file manager option: $1"
+        exit 1
+        ;;
     esac
   '';
 }
