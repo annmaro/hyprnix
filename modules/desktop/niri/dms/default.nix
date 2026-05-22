08419d64166e59f10da4648dfd0f00de9115b47f
annmaro <anandk60440@gmail.com>
2026-05-22 22:12:36 +0530

    modified:   modules/desktop/niri/dms/default.nix

--- modules/desktop/niri/dms/default.nix
+++ modules/desktop/niri/dms/default.nix
@@ -1,65 +1,69 @@
-{ config, pkgs, lib, inputs, ... }: # <-- Make sure 'inputs' is passed here
+{ config, pkgs, lib, inputs, ... }:
 
 {
-  # 1. Import the official DMS Home Manager module from your flake inputs
-  imports = [
-    inputs.dms.homeModules.dank-material-shell
+  # We define this as a reusable Home Manager module snippet
+  home-manager.sharedModules = [
+    (_: { 
+      imports = [
+        inputs.dms.homeModules.dank-material-shell
+      ];
+
+      programs.dank-material-shell = {
+        enable = true;
+        systemd.enable = true; 
+      };
+
+      home.packages = with pkgs; [
+        dgop # Required for DMS system tracking features
+      ];
+
+      xdg.configFile."DankMaterialShell/settings.json".text = builtins.toJSON {
+        configVersion = 2;
+        
+        modules = {
+          bar = true;
+          notifications = true;
+          idle = true;          
+          lockscreen = true;    
+          wallpaper = false;    
+          launcher = false;     
+          dock = false;         
+        };
+
+        theme = "catppuccin-macchiato"; 
+        dynamicTheming = false;
+
+        barConfigs = [
+          {
+            id = "default";
+            name = "Main Bar";
+            enabled = true;
+            position = "top";
+            floating = true;       
+            margin = 8;            
+            height = 30;           
+            borderRadius = 6;
+            opacity = 0.92;
+
+            network_click_action = "applet";
+            audio_click_action = "applet";
+
+            leftWidgets = [
+              "workspaceSwitcher"  
+              "focusedWindow"      
+            ];
+            centerWidgets = [
+              "clock"              
+            ];
+            rightWidgets = [
+              "systemTray"         
+              "cpuUsage"           
+              "memUsage"
+              "controlCenterButton" 
+            ];
+          }
+        ];
+      };
+    })
   ];
-
-  # 2. Use the dedicated module options instead of just 'home.packages'
-  programs.dank-material-shell = {
-    enable = true;
-    
-  # Auto-generates the background systemd services for tracking idle states
-    systemd.enable = true; 
-  };
-
-  # 4. Declarative Structural Configurations for the Shell
-  xdg.configFile."DankMaterialShell/settings.json".text = builtins.toJSON {
-    configVersion = 2;
-    
-    modules = {
-      bar = true;
-      notifications = true;
-      idle = true;          
-      lockscreen = true;    
-      wallpaper = false;    
-      launcher = false;     
-      dock = false;         
-    };
-
-    theme = "catppuccin-macchiato"; 
-    dynamicTheming = false;
-
-    barConfigs = [
-      {
-        id = "default";
-        name = "Main Bar";
-        enabled = true;
-        position = "top";
-        floating = true;       
-        margin = 8;            
-        height = 30;           
-        borderRadius = 6;
-        opacity = 0.92;
-
-        network_click_action = "applet";
-        audio_click_action = "applet";
-
-        leftWidgets = [
-          "workspaceSwitcher"  
-          "focusedWindow"      
-        ];
-        centerWidgets = [
-          "clock"              
-        ];
-        rightWidgets = [
-          "systemTray"         
-          "cpuUsage"           
-          "memUsage"
-          "controlCenterButton" 
-        ];
-      }
-    ];
-  };
 }
