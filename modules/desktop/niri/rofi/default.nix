0ce7af01355c670a32a2d167923ce53bf1b9d37c
annmaro <anandk60440@gmail.com>
2026-05-23 05:02:09 +0530

    modified:   modules/desktop/niri/rofi/default.nix

--- modules/desktop/niri/rofi/default.nix
+++ modules/desktop/niri/rofi/default.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  inherit (import ../../../../../hosts/${host}/variables.nix) terminal;
+  inherit (import ../../../../hosts/${host}/variables.nix) terminal;
 in
 {
   home-manager.sharedModules = [
