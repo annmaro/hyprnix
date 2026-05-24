7e12a5461758319c15528c697b80da5fd8ae47b3
annmaro <anandk60440@gmail.com>
2026-05-25 02:09:10 +0530

    added sops and git config

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
 {
-  description = "My NixOS Hyprland flake";
+  description = "My NixOS niri flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
-    hyprland.url = "github:hyprwm/Hyprland";
     niri  = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ modules/core/secrets/default.nix
@@ -0,0 +1,29 @@
+{ config, inputs, pkgs, ... }: 
+
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+    ./git.nix 
+  ];
+
+  home.packages = [
+    pkgs.sops
+  ];
+
+  sops = {
+    age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+    defaultSopsFile = ../../../secrets/secrets.yaml; 
+    defaultSopsFormat = "yaml";
+    
+    secrets = {
+      "private_ssh_key" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+        mode = "0600";
+      };
+      "rclone_gdrive_env" = {};
+      "git_email" = {};  
+      "git_key_id" = {}; 
+      "gemini_api_key" = {};
+    };
+  };
+}
