ae77436a7cbabc175ccdbe64e8406eec5cdcfa06
annmaro <anandk60440@gmail.com>
2026-05-26 04:35:35 +0530

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
-    niri-nix.url = "git+https://codeberg.org/BANanaD3V/niri-nix";
+    niri-flake.url = "github:sodiboo/niri-flake";
 
     home-manager = {
       url = "github:nix-community/home-manager";
