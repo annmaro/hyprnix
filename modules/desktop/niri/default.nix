d0d3372abfd496a984486c160d03a69b0fbd800c
annmaro <anandk60440@gmail.com>
2026-05-26 03:03:05 +0530

    modified:   modules/desktop/niri/default.nix

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -38,7 +38,6 @@
     trusted-public-keys = [ "niri-nix.cachix.org-1:SvFtqpDcf7Sm1SMJdby1/+Y+6f3Yt3/3PMcSTKPJNJ0=" ];
   };
 
-  nixpkgs.overlays = [ inputs.niri-nix.overlays.niri-nix ];
 
   # Set Niri as the default session for your Display Manager
   services.displayManager.defaultSession = "niri";
