9f0aaa92200dd9d427aab9acffbfa0f2b8a78ac7
annmaro <anandk60440@gmail.com>
2026-05-26 02:26:45 +0530

    modified:   flake.lock 	changed niri flake input from sodiboo to /BANanaD3V/niri-nix:   flake.nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
-    niri  = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    niri-nix.url = "git+https://codeberg.org/BANanaD3V/niri-nix";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
