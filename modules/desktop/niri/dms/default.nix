fb85643b305e8c1b415c528d1b64d3b2bb38a195
annmaro <anandk60440@gmail.com>
2026-05-27 16:39:03 +0530

    modified:   modules/desktop/niri/dms/default.nix

--- modules/desktop/niri/dms/default.nix
+++ modules/desktop/niri/dms/default.nix
@@ -58,7 +58,7 @@
         # 🎨 NATIVE WIDGET STYLING & BORDERS (Root Level)
         # ==========================================
         borderEnabled = true;               # Enables native widget/bar borders
-        borderColor = "outline";          # Custom blue outline
+        borderColor = "#89b4fa";          # Custom blue outline
         borderThickness = 2;                # Set to 2px
         borderOpacity = 1.0;                # Fully opaque outline
         widgetTransparency = 0.90;          # Set widget background to 90% opacity for a subtle see-through effect  
