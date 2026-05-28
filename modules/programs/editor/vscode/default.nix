cef09b7018a45273d43c98ddcf30f60016e1e57a
annmaro <anandk60440@gmail.com>
2026-05-29 04:21:31 +0530

    modified:   modules/programs/editor/vscode/default.nix

--- modules/programs/editor/vscode/default.nix
+++ modules/programs/editor/vscode/default.nix
@@ -21,7 +21,7 @@
         package = pkgs.vscode;
         profiles.default = {
           extensions = [
-            gruvbox-black
+            mandeepsran.gruvbox-black
           ];
           keybindings = [
             {
