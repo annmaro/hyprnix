ce388651225d28fd2df24864e6dc8bf096c0e1d2
annmaro <anandk60440@gmail.com>
2026-05-29 02:20:13 +0530

    added gruvbox theme, onepunch:   modules/programs/media/spicetify/default.nix

--- modules/programs/media/spicetify/default.nix
+++ modules/programs/media/spicetify/default.nix
@@ -23,8 +23,8 @@
         # configure spicetify :)
         programs.spicetify = {
           enable = true;
-          theme = spicePkgs.themes.gruvbox;
-          colorScheme = "amoled";
+          theme = spicePkgs.themes.onepunch;
+          colorScheme = "dark";
           # windowManagerPatch = config.programs.hyprland.enable;
           enabledExtensions = with spicePkgs.extensions; [
             adblock
