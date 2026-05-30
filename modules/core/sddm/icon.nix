ea0305a63ef8689dcb732b16f1a4b2db42fb6137
annmaro <anandk60440@gmail.com>
2026-05-31 02:05:28 +0530

    modified icon image:   modules/core/sddm/icon.nix 	new file:   modules/core/sddm/nix.png

--- modules/core/sddm/icon.nix
+++ modules/core/sddm/icon.nix
@@ -1,12 +1,13 @@
 {
 
-systemd.tmpfiles.rules = 
+  systemd.tmpfiles.rules =
 
-let
-  user = "annmaro";
-  iconPath = ./kitten.png;
-in [
-  "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
-  "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${iconPath}"
-];
+    let
+      user = "annmaro";
+      iconPath = ./nix.png;
+    in
+    [
+      "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
+      "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${iconPath}"
+    ];
 }
