66b37b90a9f1207114f4abbaf32fe15aa29292ea
annmaro <anandk60440@gmail.com>
2026-05-30 23:18:53 +0530

    added the nixos logo:   modules/desktop/niri/dms/default.nix 	new file:   modules/desktop/niri/dms/nix.png

--- modules/desktop/niri/dms/default.nix
+++ modules/desktop/niri/dms/default.nix
@@ -65,7 +65,7 @@ in
           currentThemeName = "custom"; # Use "custom" to apply your custom theme file specified below
           customThemeFile = "${config.home.homeDirectory}/.config/DankMaterialShell/themes/amoledBlack/theme.json"; # Path to your custom theme file for consistent theming across all DMS widgets. Make sure this file exists and contains your desired color settings.
 
-          profileImage = "${config.home.homeDirectory}/.config/DankMaterialShell/nix.png"; # Set the path to your profile image for display in the overview and other DMS components. Make sure the image exists at this location and is in a supported format (e.g., PNG, JPEG).
+          profileImage = "${./nix.png}"; # Set the path to your profile image for display in the overview and other DMS components. Make sure the image exists at this location and is in a supported format (e.g., PNG, JPEG).
 
           widgetBackgroundColor = "s"; # Use DMS's color tokens for consistent theming
           widgetColorMode = "colorful"; # "colorful" to use theme colors, "default" for a more neutral look
