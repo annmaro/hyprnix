dc75c40ab020935b72567724060d999c54e289e5
annmaro <anandk60440@gmail.com>
2026-06-02 22:53:38 +0530

    modified:   modules/core/system/default.nix

--- modules/core/system/default.nix
+++ modules/core/system/default.nix
@@ -75,17 +75,17 @@ in
       "en_IN/UTF-8"
       "C.UTF-8/UTF-8"
     ];
-  extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
-  };
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_IN";
+      LC_IDENTIFICATION = "en_IN";
+      LC_MEASUREMENT = "en_IN";
+      LC_MONETARY = "en_IN";
+      LC_NAME = "en_IN";
+      LC_NUMERIC = "en_IN";
+      LC_PAPER = "en_IN";
+      LC_TELEPHONE = "en_IN";
+      LC_TIME = "en_IN";
+    };
   };
   environment.variables = {
     NIXOS_OZONE_WL = "1";
@@ -102,6 +102,7 @@ in
 
   console.keyMap = "${consoleKeymap}";
   nixpkgs = {
+    overlays = builtins.attrValues overlays;
     config = {
       allowUnfree = true;
       # allowUnfreePredicate = _: true;
