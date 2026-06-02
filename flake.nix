cf2c01f2b3ee506d41d08bc87be81d4a778cadeb
annmaro <anandk60440@gmail.com>
2026-06-02 20:44:38 +0530

    added dms:   flake.nix

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
