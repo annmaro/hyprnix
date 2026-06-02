0d2e8f970f09978e9162713994c4ff8db0985b96
annmaro <anandk60440@gmail.com>
2026-06-02 23:50:48 +0530

    modified:   modules/programs/media/spicetify/default.nix

--- modules/programs/media/spicetify/default.nix
+++ modules/programs/media/spicetify/default.nix
@@ -8,26 +8,24 @@
     (
       { pkgs, ... }:
       let
-        # 1. Grab the theme packages from the input normally
+        # Safely pull the themes/extensions directly from the flake packages
         spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-
-        # 2. Build a local unfree package set just for pulling the core spotify binary
-        unfreePkgs = import inputs.nixpkgs {
-          inherit (pkgs) system;
-          config.allowUnfree = true;
-        };
       in
       {
-        # import the flake's module for your system
+        # Import the flake's module for your system
         imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-        # configure spicetify :)
+        # Force the local Nixpkgs instance evaluating this module to accept Spotify
+        nixpkgs.config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (lib.getName pkg) [
+            "spotify"
+            "spicetify"
+          ];
+
+        # Configure spicetify :)
         programs.spicetify = {
           enable = true;
-
-          # 3. Force the module to build the final client using our unfree package set
-          spicetifyPackage = spicePkgs.spicetify.override { pkgs = unfreePkgs; };
-
           theme = spicePkgs.themes.onepunch;
           colorScheme = "dark";
 
