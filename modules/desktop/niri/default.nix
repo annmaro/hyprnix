893971506fda7034f744505ad6797fc8a01b7325
annmaro <anandk60440@gmail.com>
2026-06-03 21:43:36 +0530

    modified:   modules/desktop/niri/default.nix

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -310,7 +310,7 @@ in
               "Mod+Right" { focus-column-right; }
               "Mod+H" { focus-column-left; }
               "Mod+L" { focus-column-right; }
-              "Mod+S" { spawn "sh" "-c" "niri msg action toggle-overview && killall -SIGUSR1 waybar && dms ipc call overview toggle"; }
+              "Mod+S" { spawn "sh" "-c" "niri msg action toggle-overview"; }
               "Mod+Ctrl+Left" { move-column-left; }
               "Mod+Ctrl+Right" { move-column-right; }
               
