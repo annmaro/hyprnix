e7f2afcb09385175e18efc789320e75ebe7becb9
annmaro <anandk60440@gmail.com>
2026-06-13 13:58:33 +0530

    removed rofi because already installed:   modules/programs/browser/vimb/default.nix

--- modules/programs/browser/vimb/default.nix
+++ modules/programs/browser/vimb/default.nix
@@ -11,7 +11,6 @@
           vimb
           rbw
           rofi-rbw
-          rofi-wayland
           gst_all_1.gstreamer
           gst_all_1.gst-plugins-base
           gst_all_1.gst-plugins-good
