b5f44a28c0656eaecb4e3f1592ebeae1ce366e3f
annmaro <anandk60440@gmail.com>
2026-07-02 22:36:08 +0530

    SSH client configuration:   modules/core/ssh/default.nix

--- modules/core/ssh/default.nix
+++ modules/core/ssh/default.nix
@@ -10,4 +10,12 @@
       PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
     };
   };
+
+  # Configure SSH client to use port 443 for GitHub to bypass firewall restrictions
+  programs.ssh.extraConfig = ''
+    Host github.com
+      Hostname ssh.github.com
+      Port 443
+      User git
+  '';
 }
